8d395dbd649f3e14ce23065dac0927fa227362f0
Shreyansh Singh <63132919+krishna9singh@users.noreply.github.com>
2024-06-12 10:53:12 +0530

    1

--- index.js
+++ index.js
@@ -70,6 +70,9 @@ admin.initializeApp({
 // roomManager
 let rooms = [];
 
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
 function addUserToRoom(roomName, userId, socketId) {
   let room = rooms.find((r) => r.name === roomName);
 
@@ -852,6 +855,34 @@ io.on("connection", (socket) => {
   socket.on("leaveRoom", ({ roomId, userId }) => {
     socket.leave(roomId);
     removeUserFromRoom(roomId, userId);
+  });
+
+  //video calling
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  //call
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
 
   socket.on("disconnect", () => {
